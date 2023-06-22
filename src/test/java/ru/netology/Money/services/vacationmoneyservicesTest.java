d5b1e97275103ed6a2a60a12d5c42f5893457ee1
Levashov Andrey <levashov.andrei2018@gmail.com>
2023-06-22 23:05:50 +0300

    Name change to vacationmoneyservicesTest

--- src/test/java/ru/netology/Money/services/vacationmoneyservicesTest.java
+++ src/test/java/ru/netology/Money/services/vacationmoneyservicesTest.java
@@ -2,14 +2,15 @@ package ru.netology.Money.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 
-public class vacationmoneyservices {
+public class vacationmoneyservicesTest {
     @ParameterizedTest
-    @CsvSource({
-            "10000, 3000, 20000, 3",
-            "100000, 60000, 150000, 2"})
+    @CsvFileSource(files="src/test/resources/money.csv")
+   // @CsvSource({
+   //   "10000, 3000, 20000, 3",
+  //  "100000, 60000, 150000, 2"})
 
     public void calculate(int income, int expenses, int threshold, int expected) {
         Vacationmoney services = new Vacationmoney();
